6a7e12bf7a1f7d21c10429d26dc257b90b5fc016
Vinicius Presa <viniciuspre@gmail.com>
2025-03-19 12:12:08 -0300

    feat(backend) em desenvolvimento

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { DbModule } from './db/db.module';
+import { ProjetoModule } from './projeto/projeto.module';
+import { TecnologiaModule } from './tecnologia/tecnologia.module';
+
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [DbModule, ProjetoModule, TecnologiaModule],
+  controllers: [AppController],  
 })
 export class AppModule {}
