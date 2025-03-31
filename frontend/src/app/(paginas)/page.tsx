fe7575822223f0f8731a31c89387cf28d915e447
Vinicius Presa <viniciuspre@gmail.com>
2025-03-31 16:05:44 -0300

    teste 2

--- frontend/src/app/(paginas)/page.tsx
+++ frontend/src/app/(paginas)/page.tsx
@@ -5,8 +5,6 @@ import Container from "@/components/shared/container";
 import { obterProjetos } from "@/function/projetos";
 import { obterTecnologias } from "@/function/tecnologias";
 
-//export const dynamic = 'force-dynamic'
-//export const fetchCache = 'force-no-store'
 
 export default async function Home() {
   const tecnologias = await obterTecnologias()
